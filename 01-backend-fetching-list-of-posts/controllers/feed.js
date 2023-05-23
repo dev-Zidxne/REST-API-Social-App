abccbae86d69055a9b4fbb84506a6fbde760c822
Zidane Innis <zidaneinnis@gmail.com>
2023-05-23 15:52:00 -0500

    Fetching a single post

--- 01-backend-fetching-list-of-posts/controllers/feed.js
+++ 01-backend-fetching-list-of-posts/controllers/feed.js
@@ -1,45 +1,69 @@
-const { validationResult} = require('express-validator');
+const { validationResult } = require('express-validator');
 
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: "1",
-        title: "First Post",
-        content: "This is the first post!",
-        imageUrl: "images/coffee.jpeg",
-        creator: {
-          name: "Zidane",
-        },
-        createdAt: new Date(),
-      },
-    ],
-  });
+	Post.find()
+		.then((posts) => {
+			res.status(200).json({
+				message: 'Fetched posts successfully',
+				posts: posts,
+			});
+		})
+		.catch((err) => {
+			if (!err.statusCode) {
+				err.statusCode = 500;
+			}
+			next(err);
+		});
 };
 
 exports.createPost = (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(422).json({ message: "Validation failed, please check input ", errors: errors.array() });
-  }
-  const title = req.body.title;
-  const content = req.body.content;
-  const post = new Post({
-    title: title,
-    content: content,
-    imageUrl: "images/coffee.jpeg",
-    creator: {name: "Zidane"},
-    });
-    post.save().then(result =>{
-      console.log(result);
-      res.status(201).json({
-        message: "Post created successfully!",
-        post: post,
-      
-      });
-    }).catch(err => {
-      console.log(err);
-    })
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		const error = new Error('Validation failed, please check input ');
+		error.statusCode = 422;
+		throw error;
+	}
+	const title = req.body.title;
+	const content = req.body.content;
+	const post = new Post({
+		title: title,
+		content: content,
+		imageUrl: 'images/coffee.jpeg',
+		creator: { name: 'Zidane' },
+	});
+	post
+		.save()
+		.then((result) => {
+			res.status(201).json({
+				message: 'Post created successfully!',
+				post: result,
+			});
+		})
+		.catch((err) => {
+			if (!err.statusCode) {
+				err.statusCode = 500;
+			}
+			next(err);
+		});
+};
+
+exports.getPost = (req, res, next) => {
+	const postId = req.params.postId;
+	Post.findById(postId)
+		.then((post) => {
+			if (!post) {
+				const error = new Error('Post not found');
+				error.statusCode = 404;
+				throw error;
+			}
+			res.status(200).json({ messgae: 'Post fetched', post: post });
+		})
+		.catch((err) => {
+			if (!err.statusCode) {
+				err.statusCode = 500;
+			}
+			next(err);
+		});
 };
